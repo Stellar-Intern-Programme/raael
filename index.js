82bd27d2f7a315ff00ab0af463783a568f9ad229
Hamburda <rares.hamburda@gmail.com>
2022-06-23 16:21:44 +0300

    java scrpt

---
+++ index.js
@@ -0,0 +1,14 @@
+function createImg(){
+    const div=document.querySelector(".imgs")
+    const img=document.createElement("img")
+    img.setAttribute("src", "resurse/City.svg")
+    const divP=document.createElement("div")
+    divP.setAttribute("class","Poze")
+    const p=document.createElement("p")
+    p.textContent = 'New York, USA'
+    p.setAttribute("class","City")
+    divP.appendChild(img)
+    divP.appendChild(p)
+    div.appendChild(divP)
+
+}
