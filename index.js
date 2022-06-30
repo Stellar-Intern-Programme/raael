10031917c6ddfb8fb8f6ff83ad2cb9aed6437184
Hamburda <rares.hamburda@gmail.com>
2022-06-30 14:26:49 +0300

    sustache

--- index.js
+++ index.js
@@ -13,117 +13,145 @@
 
 // }
 
+// import weather from"./weather.json" assert {type: "json"}
 
-function openModal(){
-    const modal=document.querySelector(".modal")
-    modal.style.display="flex";
-    const overlay=document.querySelector(".overlay")
-    overlay.style.display="flex";
-    const photoInput = document.getElementById("url")
-    photoInput.addEventListener("change", onInputChange)
-    const defImage = document.createElement("img")
-    defImage.src="resurse/Image.svg";
-    defImage.className = "defImage"
-    const preview = document.querySelector(".preview")
-    preview.replaceChildren(defImage)
+let weather;
+fetch("./weather.json")
+  .then((res) => res.json())
+  .then((data) => (weather = data));
+renderWeather;
+
+function openModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = "flex";
+  const overlay = document.querySelector(".overlay");
+  overlay.style.display = "flex";
+  const photoInput = document.getElementById("url");
+  photoInput.addEventListener("change", onInputChange);
+  const defImage = document.createElement("img");
+  defImage.src = "resurse/Image.svg";
+  defImage.className = "defImage";
+  const preview = document.querySelector(".preview");
+  preview.replaceChildren(defImage);
 }
 
-function closeModal(){
-    const modal=document.querySelector(".modal")
-    modal.style.display="none";
-    const overlay=document.querySelector(".overlay")
-    overlay.style.display="none";
-    const url=document.getElementById("url")
-    url.value = "";
-    const city=document.getElementById("city")
-    city.value = "";
-    const background = document.querySelector(".preview");
-    background.style.backgroundImage = `unset`
+function closeModal() {
+  const modal = document.querySelector(".modal");
+  modal.style.display = "none";
+  const overlay = document.querySelector(".overlay");
+  overlay.style.display = "none";
+  const url = document.getElementById("url");
+  url.value = "";
+  const city = document.getElementById("city");
+  city.value = "";
+  const background = document.querySelector(".preview");
+  background.style.backgroundImage = `unset`;
 }
 
-function onInputChange(event){
+function onInputChange(event) {
   const photo = event.target.value;
   const background = document.querySelector(".preview");
   const defaultImage = document.querySelector(".defImage");
   background.removeChild(defaultImage);
-  background.style.backgroundImage = `url( '${photo}')`
-  background.style.backgroundSize = `cover`
-  background.style.backgroundRepeat = `none`
+  background.style.backgroundImage = `url( '${photo}')`;
+  background.style.backgroundSize = `cover`;
+  background.style.backgroundRepeat = `none`;
 }
 
-let arrayOfCities=JSON.parse(localStorage.getItem("cities"))||[] 
+let arrayOfCities = JSON.parse(localStorage.getItem("cities")) || [];
 
-function addCities(){
-    const cityName=document.getElementById("city").value
-    const photoUrl=document.getElementById("url").value
-    arrayOfCities.push({name:cityName,url:photoUrl})
-   
-    localStorage.setItem("cities",JSON.stringify(arrayOfCities))
-    init()
-    
+function addCities() {
+  const cityName = document.getElementById("city").value;
+  const photoUrl = document.getElementById("url").value;
+  arrayOfCities.push({ name: cityName, url: photoUrl });
+
+  localStorage.setItem("cities", JSON.stringify(arrayOfCities));
+  init();
 }
-window.addEventListener('load', (event) => {
-    init()
+window.addEventListener("load", (event) => {
+  init();
+  renderWeather();
+});
+
+function init() {
+  const div = document.querySelector(".imgs");
+  div.innerHTML = "";
+  arrayOfCities.forEach((city) => {
+    renderCities(city);
   });
-
-
-function init(){
-    const div=document.querySelector(".imgs")
-    div.innerHTML=""
-    arrayOfCities.forEach((city)=>{
-        renderCities(city)
-    })
-    const pOras = document.getElementById("Oras")
-    pOras.innerHTML = arrayOfCities[0].name
+  const pOras = document.getElementById("Oras");
+  pOras.innerHTML = arrayOfCities[0].name;
 }
 
-
-
-function renderCities(city){
-    const div=document.querySelector(".imgs")
-    const img=document.createElement("img")
-    // <img>
-    img.setAttribute("src", city.url)   
-    img.style.objectFit = `cover`
-    const divP=document.createElement("div")
-    divP.setAttribute("class","Poze")
-    divP.onclick = () => LMAO_XD(city)
-    const p=document.createElement("p")
-    p.textContent = city.name;
-    p.setAttribute("class","City")
-    divP.appendChild(img)
-    divP.appendChild(p)
-    div.appendChild(divP)
-    closeModal() 
+function renderCities(city) {
+  const div = document.querySelector(".imgs");
+  const img = document.createElement("img");
+  // <img>
+  img.setAttribute("src", city.url);
+  img.style.objectFit = `cover`;
+  const divP = document.createElement("div");
+  divP.setAttribute("class", "Poze");
+  divP.onclick = () => LMAO_XD(city);
+  const p = document.createElement("p");
+  p.textContent = city.name;
+  p.setAttribute("class", "City");
+  divP.appendChild(img);
+  divP.appendChild(p);
+  div.appendChild(divP);
+  closeModal();
 }
 
-function LMAO_XD(city){
-    selectedCity = city;
-    const pOras = document.getElementById("Oras")
-    pOras.innerHTML = city.name;
-   
+function LMAO_XD(city) {
+  selectedCity = city;
+  const pOras = document.getElementById("Oras");
+  pOras.innerHTML = city.name;
 }
 
-function deleteCity(){
-  arrayOfCities = arrayOfCities.filter((city)=> {
-   
-    return city.name !== selectedCity.name
-  })
-  localStorage.setItem("cities",JSON.stringify(arrayOfCities))
+function deleteCity() {
+  arrayOfCities = arrayOfCities.filter((city) => {
+    return city.name !== selectedCity.name;
+  });
+  localStorage.setItem("cities", JSON.stringify(arrayOfCities));
 
-  init()
+  init();
 }
 
 let selectedCity = arrayOfCities[0];
-function regenerate(event){
-  if(event.value==="")
-  init()
+function regenerate(event) {
+  if (event.value === "") init();
 }
 
-function search(event){
-  const imgs=document.getElementsByClassName("imgs")[0]
-searchArray = arrayOfCities.filter(e => e.name.toLowerCase().includes(event.value))
-imgs.innerHTML=""
-searchArray.forEach(e=>renderCities(e))
-regenerate(event)
+function search(event) {
+  const imgs = document.getElementsByClassName("imgs")[0];
+  searchArray = arrayOfCities.filter((e) =>
+    e.name.toLowerCase().includes(event.value)
+  );
+  imgs.innerHTML = "";
+  searchArray.forEach((e) => renderCities(e));
+  regenerate(event);
+}
+function renderWeather() {
+  Object.keys(weather).forEach((item) => {
+    const p = document.createElement("p");
+
+    const divP = document.querySelector(".Buttons");
+    p.textContent = item;
+
+    p.setAttribute("class", "Buttons");
+
+    divP.appendChild(p);
+    p.addEventListener("click", () =>{
+      const children=divP.children
+      for(let i=0; i<children.length;i++)
+      {
+        children[i].className="Buttons"
+      }
+      console.log(divP)
+      p.classList="Buttons active"
+      renderWeatherData(item)});
+      
+  });
+}
+function renderWeatherData(){
+
 }
